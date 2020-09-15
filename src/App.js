64781e1270c15cbad599f121d8efd92a61a58edc
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-15 17:09:43 -0400

    Render TodoList from App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TodoList from './TodoList';
+
 
 function App() {
   return (
     <div className="App">
-    Hello
+    <TodoList/>
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,1 +1,5 @@
 import React from 'react';
+const TodoList = ()=><h1>Im a to do lists</h1>
+
+
+export default TodoList;
