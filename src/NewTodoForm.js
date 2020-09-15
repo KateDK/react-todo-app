b5316aec32f575b9050a8bc64b1dc8efacfef8bc
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-15 17:16:02 -0400

    Add starting NewTodoForm component code

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+class NewTodoForm extends React.Component{
+  state={
+
+  }
+  render(){
+    return(
+      <div className="NewTodoForm">
+        Im a form!
+      </div>
+    )
+  }
+}
+
+export default NewTodoForm;
