7bbe1cb85ea51316f95079c811575ed0a82421e7
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-16 16:32:16 -0400

    NewTodoForm - add form
    -two way binding

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 class NewTodoForm extends React.Component{
   state={
+    todo:"",
+  }
 
+  handleChange = (event)=>{
+    this.setState({todo: event.target.value});
   }
   render(){
     return(
       <div className="NewTodoForm">
-        Im a form!
+        <form>
+          <label>New todo: </label>
+          <input name="todo" value={this.state.todo} type="text" onChange={this.handleChange}/>
+        </form>
       </div>
     )
   }
