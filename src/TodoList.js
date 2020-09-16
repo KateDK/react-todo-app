6b303378517a283d7cb27e7baceda95860754427
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-16 16:54:31 -0400

    add uuid to project
    add keys to todos map in TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import NewTodoForm from './NewTodoForm';
 import Todo from './Todo';
+import { v4 as uuidv4 } from 'uuid';
 
-class TodoList extends React.Component{
+class TodoList extends React.Component {
   state = {
-    todos:[]
-  }
-  render(){
-    const {todos} = this.state;
+    todos: [],
+  };
+  render() {
+    const { todos } = this.state;
     return (
       <div className="TodoList">
-        <NewTodoForm/>
-        {todos.map(todo=><Todo todo={todo}/>)}
+        <NewTodoForm />
+        {todos.map((todo) => (
+          <Todo todo={todo} key={uuidv4()} />
+        ))}
       </div>
-    )
+    );
   }
 }
-
 
 export default TodoList;
