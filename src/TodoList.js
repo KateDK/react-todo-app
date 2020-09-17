4499100158d9ef525123586e477d71aa90adde0e
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-17 17:15:28 -0400

    Create deleteTodo methos
    -pass to Todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,13 +11,24 @@ class TodoList extends React.Component {
     this.setState({ todos: [...this.state.todos, newTodo] });
   };
 
+  deleteTodo = (todoId) => {
+    const { todos } = this.state;
+    const filteredTodos = todos.filter((elm) => elm.id !== todoId);
+    this.setState({ todos: filteredTodos });
+  };
+
   render() {
     const { todos } = this.state;
     return (
       <div className="TodoList">
         <NewTodoForm addTodo={this.addTodo} />
         {todos.map((item) => (
-          <Todo todo={item.todo} key={item.id} id={item.id} />
+          <Todo
+            todo={item.todo}
+            key={item.id}
+            id={item.id}
+            deleteTodo={this.deleteTodo}
+          />
         ))}
       </div>
     );
