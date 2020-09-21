8f340b4f3d4efa2fcf97484b638436b749f0ec42
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-21 19:17:42 -0400

    change Todo to a class

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './Todo.css';
+import NewTodoForm from './NewTodoForm';
 
-const Todo = (props) => (
-  <div className="Todo">
-    <h2>{props.todo}</h2>
-    <button onClick={() => props.deleteTodo(props.id)}>X</button>
-    <button>Edit</button>
-  </div>
-);
+class Todo extends React.Component {
+  state = {
+    editMode: false,
+  };
+  handleClcick = () => {
+    this.setState({ editMode: !this.state.editMode });
+  };
+  render() {
+    const { deleteTodo, todo, id, addTodo } = this.props;
+    const { editMode } = this.state;
+    return (
+      <div className="Todo">
+        {editMode ? (
+          <NewTodoForm
+            addTodo={addTodo}
+            todoText={todo}
+            buttonText="Save"
+            label="Edit Todo "
+          />
+        ) : (
+          <span>
+            <h2>{todo}</h2>
+            <button onClick={() => deleteTodo(id)}>X</button>
+            <button onClick={this.handleClcick}>Edit</button>
+          </span>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Todo;
