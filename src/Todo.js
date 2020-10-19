ab6aaf88295581b2f370b6fabc7e906912fad031
Kate Dubitski <kate.dubitski@gmail.com>
2020-10-19 17:59:06 -0400

    Refactor className assignment to separate method

--- src/Todo.js
+++ src/Todo.js
@@ -15,13 +15,29 @@ class Todo extends React.Component {
     toggleCompleteTodo(todo);
   };
 
+  makeClassNames = () => {
+    const { todo } = this.props;
+    let todoClass;
+    let todoTextClass;
+    if(todo.completed){
+      todoClass="Todo completedTodo";
+      todoTextClass='Todo-task completed'
+    }else{
+      todoClass="Todo";
+      todoTextClass='Todo-task'
+    }
+    return {todoClass,todoTextClass}
+  }
+
   render() {
     const { deleteTodo, todo, updateTodo } = this.props;
-    const todoTextClass = todo.completed ? 'Todo-task completed ' : 'Todo-task';
+    const classNames = this.makeClassNames();
+    const todoClass = classNames.todoClass;
+    const todoTextClass = classNames.todoTextClass;
     const todoToggle = todo.completed ? <i class="fas fa-check-square"></i> : <i class="far fa-check-square"></i>
     const { editMode } = this.state;
     return (
-      <div className="Todo">
+      <div className={todoClass}>
         {editMode ? (
           <span>
             <EditTodoForm
