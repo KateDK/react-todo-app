4dc7dbbf3066dc90880f6ed425b4a894b8cd476b
Kate Dubitski <kate.dubitski@gmail.com>
2020-11-09 16:07:06 -0500

    Added default todos to initiate list on BE

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,8 +1,11 @@
 exports.todos = (req, res) => {
   res.json({
     todos: [
-      { todoText: 'eat', id: 1, completed: false, important: false },
-      { todoText: 'vote', id: 2, completed: false, important: false },
+      { todoText: 'Create new todo', id: 1, completed: false, important: false },
+      { todoText: 'Mark this todo as completed', id: 2, completed: false, important: false },
+      { todoText: 'Edit this Todo', id: 3, completed: false, important: false },
+      { todoText: 'Delete this todo', id: 4, completed: false, important: false },
+      { todoText: 'Make this todo important', id: 5, completed: false, important: false }
     ],
   });
 };
