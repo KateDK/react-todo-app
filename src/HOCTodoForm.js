0a4cd86c59f1646b861b12ec353ef758113d93be
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-21 19:20:26 -0500

    create Todo form higher order component
    -HOCTodoForm created
    -Logic added

---
+++ src/HOCTodoForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const makeNewBlankToDo = () => {
+  return { todoText: '', completed: false, important: false, id: uuidv4() };
+};
+
+class TodoForm extends React.Component {
+  state = {
+    todo: this.props.todo || makeNewBlankToDo(),
+    newTodo: this.props.todo ? false : true,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      todo: { ...this.state.todo, todoText: event.target.value },
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { todo, newTodo } = this.state;
+    if (newTodo) {
+      this.props.addTodo(todo);
+      this.setState({
+        todo: makeNewBlankToDo(),
+      });
+    } else {
+      const { toggleEdit } = this.props;
+
+      this.props.updateTodo({ ...todo });
+      toggleEdit();
+    }
+  };
+
+  render() {
+    const disabled = this.state.todo.todoText === '';
+    const { component: C, defaultOnValue, ...props } = this.props;
+    return (
+      <C
+        disabled={disabled}
+        todo={this.state.todo}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleChange}
+        {...props}
+      />
+    );
+  }
+}
+
+export function HOCTodoForm(component, optionsObj) {
+  return function (props) {
+    return (
+      <TodoForm
+        {...props}
+        component={component}
+        buttonText={optionsObj.buttonText}
+        labelText={optionsObj.labelText}
+        placeholder={optionsObj.placeholder}
+      />
+    );
+  };
+}
