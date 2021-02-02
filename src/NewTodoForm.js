f3928f8a53d1d402c8404da492f3c5a5e706e3c1
Kate Dubitski <kate.dubitski@gmail.com>
2021-02-01 19:09:10 -0500

    resolve handleSubmit error in NewTodoForm

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TodoForm from './HOCTodoForm';
 
-const NewTodoForm = () => {
+const NewTodoForm = (props) => {
   return (
     <TodoForm
+      addTodo={props.addTodo}
       render={({ todo, handleSubmit, handleChange }) => {
         const disabled = todo.todoText === '';
         return (
